4265b5470d1d3e509920566eac73a4d34de451ab
venkat-devish <venkatsuryakiranv@gmail.com>
2022-12-11 22:40:29 +0530

    add: selected details

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,25 +1,16 @@
 import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
-import movieApi from "../../common/apis/movieApi";
-import { apiKey } from "../../common/apis/apiKey";
+
 import "./Home.scss";
 import { useDispatch } from "react-redux";
-import { addMovies } from "../../features/movies/moviesSlice";
+import { fetchMovies, fetchShows } from "../../features/movies/moviesSlice";
 
 const Home = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    const movieText = "Harry";
-    const fetchMovies = async () => {
-      const response = await movieApi
-        .get(`?apiKey=${apiKey}&s=${movieText}&type=movie`)
-        .catch((err) => {
-          console.log(err);
-        });
-      dispatch(addMovies(response.data));
-    };
-    fetchMovies();
-  }, []);
+    dispatch(fetchMovies());
+    dispatch(fetchShows());
+  }, [dispatch]);
   return (
     <>
       <div className="banner-img"></div>
--- src/components/MovieListing/MovieListing.jsx
+++ src/components/MovieListing/MovieListing.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./MovieListing.scss";
 import { useSelector } from "react-redux";
-import { getAllMovies } from "../../features/movies/moviesSlice";
+import { getAllMovies, getAllShows } from "../../features/movies/moviesSlice";
 import MovieCard from "../MovieCard/MovieCard";
 
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
+  const shows = useSelector(getAllShows);
   const renderMovies =
     movies.Response === "True" ? (
       movies.Search.map((movie, index) => (
@@ -17,11 +18,24 @@ const MovieListing = () => {
       </div>
     );
 
+  const renderShows =
+    shows.Response === "True" ? (
+      shows.Search.map((show, index) => <MovieCard key={index} data={show} />)
+    ) : (
+      <div className="shows-error">
+        <h3>{shows.error}</h3>
+      </div>
+    );
+
   return (
     <div className="movie-wrapper">
       <div className="movie-list">
         <h2>Movies</h2>
         <div className="movie-container">{renderMovies}</div>
+      </div>
+      <div className="show-list">
+        <h2>Shows</h2>
+        <div className="movie-container">{renderShows}</div>
       </div>
     </div>
   );
