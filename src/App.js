228a5f68292642a6a599bba3d93e0ccf87aaa94b
venkat-devish <venkatsuryakiranv@gmail.com>
2023-01-02 17:25:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
 import "./App.scss";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Home from "./components/Home/Home";
-import MovieDetails from "./components/MovieDetails/MovieDetails";
-import PageNotFound from "./components/PageNotFound/PageNotFound";
-import Footer from "./components/Footer/Footer";
+import Counter from "./components/Counter";
 
 function App() {
-  return (
-    <div className="app">
-      <Router>
-        <Header></Header>
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/movie/:imdbID" element={<MovieDetails />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </div>
-        <Footer></Footer>
-      </Router>
-    </div>
-  );
+  return <main></main>;
 }
 
 export default App;
--- src/common/apis/apiKey.js
+++
@@ -1,1 +0,0 @@
-export const apiKey = "c07de144";
